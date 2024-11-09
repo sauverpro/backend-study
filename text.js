eddb0680e0fb842821d112cefcd5892f9794d594
sauverpro <unsauvechriss@gmail.com>
2024-11-09 17:55:54 +0200

    js study examples

---
+++ text.js
@@ -0,0 +1,11 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// fruits[2] = ("lemon")
+console.log(fruits.slice(1));
+
+const object = {
+    names: "sauveur",
+    email: "sauveur@gmail.com"
+}
+
+object["email"] = "tuyizere@gmail.com"
